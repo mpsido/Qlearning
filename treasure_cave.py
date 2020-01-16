e896fce1975e5f521ddcea7e36f0754f6ffa2830
mpsido <1979371837@qq.com>
2020-01-16 15:55:38 +0800

    treasure_cave Changing for a bigger map with two enemies

--- treasure_cave.py
+++ treasure_cave.py
@@ -26,10 +26,11 @@ class TreasureCave(Game):
 
     def __init__(self, verbose=False):
         self.treasure_map=np.array([
-            [".",".",".","."],
-            [".","0",".","0"],
-            [".",".",".","0"],
-            ["0",".",".","T"]
+            ['.','.','.','.','.','.','.'],
+            ['.','.','0','0','.','0','.'],
+            ['0','.','.','0','.','.','.'],
+            ['.','.','.','.','.','0','.'],
+            ['0','.','.','.','.','T','.']
         ])
         self.possible_actions=['w', 's', 'a', 'd','e']
         self.verbose=verbose
@@ -44,41 +45,41 @@ class TreasureCave(Game):
         self.done=False
         self.reward=0
         self.hero_pos=0
-        self.evil_pos=[14]
+        self.evil_pos=[24, 15]
         return (self.hero_pos, self.evil_pos), self.reward, self.done, "New game"
 
-    @staticmethod
-    def get_pos_from_coord(coord):
-        return coord[0]*4+coord[1]
+    def get_pos_from_coord(self, coord):
+        return coord[0]*self.treasure_map.shape[1]+coord[1]
 
-    @staticmethod
-    def get_coord_from_pos(pos):
-        return (int(pos/4), pos%4)
+    def get_coord_from_pos(self, pos):
+        x=int(pos/self.treasure_map.shape[1])
+        y=pos%self.treasure_map.shape[1]
+        return (x, y)
 
     def is_treasure(self, coord):
-        return self.treasure_map[coord[0]][coord[1]] == "T"
+        return self.treasure_map[coord[0]][coord[1]] == 'T'
 
     def is_hole(self, coord):
-        return self.treasure_map[coord[0]][coord[1]] == "0"
+        return self.treasure_map[coord[0]][coord[1]] == '0'
 
     def is_evil(self, coord):
         isEvil=False
         for evil_pos in self.evil_pos:
-            isEvil |= coord == TreasureCave.get_coord_from_pos(evil_pos)
+            isEvil |= coord == self.get_coord_from_pos(evil_pos)
         return isEvil
 
     def render(self):
         render_map=self.treasure_map.copy()
-        hero_coord=TreasureCave.get_coord_from_pos(self.hero_pos)
+        hero_coord=self.get_coord_from_pos(self.hero_pos)
         if not self.is_hole(hero_coord):
-            render_map[hero_coord[0]][hero_coord[1]]="X"
+            render_map[hero_coord[0]][hero_coord[1]]='X'
         for evil_pos in self.evil_pos:
-            evil_coord=TreasureCave.get_coord_from_pos(evil_pos)
-            if render_map[evil_coord[0]][evil_coord[1]] == "X":
-                render_map[evil_coord[0]][evil_coord[1]]="!"
+            evil_coord=self.get_coord_from_pos(evil_pos)
+            if render_map[evil_coord[0]][evil_coord[1]] == 'X':
+                render_map[evil_coord[0]][evil_coord[1]]='!'
             else:
-                render_map[evil_coord[0]][evil_coord[1]]="E"
-        print("\n".join([ ''.join(r) for r in render_map]))
+                render_map[evil_coord[0]][evil_coord[1]]='E'
+        print('\n'.join([ ''.join(r) for r in render_map]))
 
     def evil_move(self, hero_coord, evil_coord):
         for a in self.possible_actions:
@@ -112,9 +113,9 @@ class TreasureCave(Game):
     def play_step(self, action):
         if self.done:
             return (self.hero_pos, self.evil_pos), self.reward, self.done, "Game over"
-        hero_coord=TreasureCave.get_coord_from_pos(self.hero_pos)
+        hero_coord=self.get_coord_from_pos(self.hero_pos)
         hero_coord=self.make_move(hero_coord, action)
-        self.hero_pos=TreasureCave.get_pos_from_coord(hero_coord)
+        self.hero_pos=self.get_pos_from_coord(hero_coord)
         if self.is_hole(hero_coord):
             self.done=True
             self.reward=-1
@@ -124,9 +125,9 @@ class TreasureCave(Game):
         else:
             # todo update evil coord in state
             for i,evil_pos in enumerate(self.evil_pos):
-                evil_coord=TreasureCave.get_coord_from_pos(evil_pos)
+                evil_coord=self.get_coord_from_pos(evil_pos)
                 evil_coord=self.make_move(evil_coord, self.evil_move(hero_coord, evil_coord))
-                self.evil_pos[i]=TreasureCave.get_pos_from_coord(evil_coord)
+                self.evil_pos[i]=self.get_pos_from_coord(evil_coord)
                 if self.is_evil(hero_coord):
                     self.done=True
                     self.reward=-1
